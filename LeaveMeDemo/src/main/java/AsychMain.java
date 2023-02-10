e7516eef657c156373f616a5ea573f0f273cecf6
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-02-10 11:38:26 +0000

    demo

---
+++ LeaveMeDemo/src/main/java/AsychMain.java
@@ -0,0 +1,61 @@
+package org.djna.async;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.Session;
+
+public class AsychMain {
+    public static void main(String[] args) throws Exception {
+        String clientId = "Example";
+        if (args.length > 0){
+            clientId = args[0];
+        }
+        System.out.printf("ClientId %s%n", clientId);
+        ActiveMQConnectionFactory connFact = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        connFact.setConnectResponseTimeout(10000);
+        Connection conn = connFact.createConnection("admin", "admin");
+        conn.setClientID(clientId);
+        conn.start();
+
+        int howManyToSend = 5;
+        boolean receive = true;
+
+        if (args.length < 2) {
+            howManyToSend = 5;
+            receive = true;
+        }
+        if (args.length >= 2) {
+            try {
+                howManyToSend = Integer.valueOf(args[1]);
+            } catch (NumberFormatException nfe) {
+                usageExit("Number of messages to send, not a valid number");
+            }
+        }
+        if (args.length >= 3) {
+            receive = ("receive".equalsIgnoreCase(args[2]));
+        }
+
+        if (howManyToSend > 0) {
+            System.out.printf("Sending %d messages%n", howManyToSend);
+            new Thread(new Sender(
+                    conn.createSession(false, Session.CLIENT_ACKNOWLEDGE),
+                    "Queue.PointToPoint.OneWay.Traditional",
+                    howManyToSend)
+                  ).start();
+
+        }
+        if ( receive) {
+            System.out.printf("Receiving messages%n");
+            new Thread(new Receiver(conn.createSession(false, Session.CLIENT_ACKNOWLEDGE), "Queue.PointToPoint.OneWay.Traditional")).start();
+        }
+    }
+
+    private static void usageExit(String message) {
+        System.out.println(message);
+        System.out.println("Argument 1: a number of messages to send, 0 for none, default 5");
+        System.out.println("Optional second argument: 'receive' will start a receiver, other values will not, default to receive");
+
+    }
+
+}
